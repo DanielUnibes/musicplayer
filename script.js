86ac630ac8213bbca3c21b8ceadf72d3d73ba882
DanielUnibes <dwnete@proton.me>
2026-04-29 23:07:11 +0000

    upd

---
+++ script.js
@@ -0,0 +1,12 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBnt = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progresscontainer = document.querySelector('.progress-container')
+const tittle = document.querySelector('#tittle')
+const cover = document.querySelector('#cover')
+
+//titulos
+const songs = []
